e199c96d03535c645dc7e892b00ca22dc5687f5e
ersan <ersan@MacBook-Air-von-ersan.local>
2026-02-02 02:01:25 +0100

    feat: created contact component for more contact possibilities

---
+++ src/app/components/contact/contact.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { GlowingBtn } from "../glowing-btn/glowing-btn";
+
+@Component({
+  selector: 'app-contact',
+  imports: [GlowingBtn],
+  templateUrl: './contact.html',
+  styleUrl: './scss/contact.scss',
+})
+export class Contact {
+
+}
--- src/app/components/landing-page/landing-page.ts
+++ src/app/components/landing-page/landing-page.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Navigation } from "../navigation/navigation";
 import { Introduction } from "../introduction/introduction";
 import { QuestionsAndAnswers } from "../questions-and-answers/questions-and-answers";
+import { Contact } from "../contact/contact";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [Navigation, Introduction, QuestionsAndAnswers],
+  imports: [Navigation, Introduction, QuestionsAndAnswers, Contact],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
 })
