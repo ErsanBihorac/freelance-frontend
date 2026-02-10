7f330517857e63b6048ed6cb9917b16ba2ae029b
ersan <ersan@MacBook-Air-von-ersan.local>
2026-02-10 22:18:12 +0100

    fix: fixed img when low res

--- src/app/components/introduction/introduction.ts
+++ src/app/components/introduction/introduction.ts
@@ -18,11 +18,19 @@ export class Introduction {
   github: string = 'https://github.com/ErsanBihorac';
   mail: string = 'contact@ebihorac.de';
 
-  glowingBtn: Button = {
+  glowingBtnContact: Button = {
     text: 'Get Started Now',
     svg: {
       viewBox: '0 -960 960 960',
       path: 'm216-160-56-56 464-464H360v-80h400v400h-80v-264L216-160Z',
+    },
+  };
+
+  glowingBtnProjects: Button = {
+    text: 'View Projects',
+    svg: {
+      viewBox: '0 -960 960 960',
+      path: 'M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h240l80 80h320q33 0 56.5 23.5T880-640H447l-80-80H160v480l96-320h684L837-217q-8 26-29.5 41.5T760-160H160Zm84-80h516l72-240H316l-72 240Zm0 0 72-240-72 240Zm-84-400v-80 80Z',
     },
   };
 
